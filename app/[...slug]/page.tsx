a98a12ad10232f1b69f2116492a1f8234bbb13e1
Eric de Quartel <edequartel@protonmail.com>
2025-10-31 17:24:55 +0100

    Update page.tsx

--- app/[...slug]/page.tsx
+++ app/[...slug]/page.tsx
@@ -23,7 +23,7 @@ export default async function Page(
   const { meta, html } = await getPost(slug);
   return (
     <main>
-//      <h1>{meta.title}</h1>
+      <h1>{meta.title}</h1>
       <p><a href="/">← Back to all files</a></p>
       {meta.date && <p className="opacity-70 text-sm">{new Date(meta.date).toLocaleString()}</p>}
       <article dangerouslySetInnerHTML={{ __html: html }} />
